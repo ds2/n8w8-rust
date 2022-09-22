f15f50787e68ae3b2eee4fc75b99c078f5c30307
Dirk <472167-dstrauss@users.noreply.gitlab.com>
2022-09-22 18:29:06 +0200

    👔 agent can query disk, memory and cpu iowait stats
    Next is to make it work on a rhel ;)

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -5,6 +5,15 @@ use std::{thread, time};
 use chrono::{DateTime, Utc};
 use clap::Parser;
 use daemonize::Daemonize;
+use log::info;
+use sysinfo::{DiskExt, System, SystemExt};
+
+use nachtwacht_models::n8w8::{AgentDiscData, AgentNodeData};
+
+use crate::procstat::parse_proc_stat;
+
+mod errors;
+mod procstat;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -59,13 +68,80 @@ fn main() {
         .privileged_action(|| println!("Will enter loop now.."));
 
     match daemonize.start() {
-        Ok(_) => loop {
-            let now: DateTime<Utc> = Utc::now();
-            let date = format!("UTC now is: {}", now);
-            println!("Date is now {}", date);
-            // file.write_all(date.as_bytes()).unwrap();
-            thread::sleep(sleep_time);
-        },
+        Ok(_) => {
+            println!("Starting loop..");
+            let mut sys = System::new_all();
+            loop {
+                let now: DateTime<Utc> = Utc::now();
+                sys.refresh_all();
+                let date = format!("UTC now is: {}", now);
+                println!("Date is now {}", date);
+                // We display all disks' information:
+                let mut disk_vec: Vec<AgentDiscData> = Vec::new();
+                for disk in sys.disks() {
+                    let this_disk_data = AgentDiscData {
+                        device: disk.name().to_str().unwrap().to_string(),
+                        mountpoint: disk.mount_point().to_str().unwrap().to_string(),
+                        max_storage: disk.total_space(),
+                        free_storage: disk.available_space(),
+                        special_fields: Default::default(),
+                    };
+                    disk_vec.push(this_disk_data);
+                    // println!("{:?}", disk);
+                }
+
+                // Network interfaces name, data received and data transmitted:
+                // println!("=> networks:");
+                // for (interface_name, data) in sys.networks() {
+                //     println!(
+                //         "{}: {}/{} B",
+                //         interface_name,
+                //         data.received(),
+                //         data.transmitted()
+                //     );
+                // }
+
+                // Components temperature:
+                // println!("=> components:");
+                // for component in sys.components() {
+                //     println!("{:?}", component);
+                // }
+
+                // Display system information:
+                // println!(
+                //     "System name: {}-{}",
+                //     sys.name().unwrap(),
+                //     sys.os_version().unwrap(),
+                // );
+
+                // Display processes ID, name na disk usage:
+                // for (pid, process) in sys.processes() {
+                //     println!("[{}] {} {:?}", pid, process.name(), process.disk_usage());
+                // }
+                let cpu_proc_stats = parse_proc_stat().expect("TODO: panic message");
+                info!("This machine has {} cores!", cpu_proc_stats.len());
+                let agent_node_data = AgentNodeData {
+                    hostname: sys.host_name().unwrap(),
+                    load1: sys.load_average().one,
+                    load5: sys.load_average().five,
+                    load15: sys.load_average().fifteen,
+                    totalMemory: sys.total_memory(),
+                    usedMemory: sys.used_memory(),
+                    freeMemory: sys.free_memory(),
+                    totalSwap: sys.total_swap(),
+                    usedSwap: sys.used_swap(),
+                    freeSwap: sys.free_swap(),
+                    kernelversion: sys.kernel_version().unwrap(),
+                    cpudata: cpu_proc_stats,
+                    disks: disk_vec,
+                    os_name: sys.name().unwrap(),
+                    os_version: sys.os_version().unwrap(),
+                    special_fields: Default::default(),
+                };
+                println!("Node data is {}", agent_node_data);
+                thread::sleep(sleep_time);
+            }
+        }
         Err(e) => eprintln!("Error occurred: {}", e),
     }
 }
