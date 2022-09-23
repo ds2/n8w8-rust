2819a1edef3d7a3293967a1bd0039874c48b8766
Lexxy <lexxy23@users.noreply.github.com>
2022-09-23 12:17:46 +0200

    Create Agent for querying host for CPU/Mem/Disk (#23)
    * 🚧 agent can start
    
    And also reacts to user parameters.
    
    * 👔 agent can query disk, memory and cpu iowait stats
    
    Next is to make it work on a rhel ;)
    
    * 📝 update docs to show how to generate RPM
    
    Also instructions on how to test it via Almalinux ;)
    
    * ⚗️ enhance flow for generating the rpm
    
    * 👷 fix github agent release
    
    * 👷 publish artifacts?
    
    * 👷 disable concurrent builds
    
    * 🚧 use the old name from the c++ agent
    
    And also try to fix build
    
    * 🚧 fix wrong project
    
    * 💚 fix wrong path
    
    * 💚 fix wrong path 2 (the target dir)
    
    * 🚧 add debian build for agent
    
    * 🚧 build debian package
    
    * 📄 put all under GPLv3 except the website/frontend.
    
    * 🔨 define toolchain(s) to use
    
    * ⚰️ remove comments
    
    Co-authored-by: Dirk <472167-dstrauss@users.noreply.gitlab.com>

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,117 @@
+use std::fs::File;
+use std::path::Path;
+use std::{thread, time};
+
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use daemonize::Daemonize;
+use log::info;
+use sysinfo::{DiskExt, System, SystemExt};
+
+use nachtwacht_models::n8w8::{AgentDiscData, AgentNodeData};
+
+use crate::procstat::parse_proc_stat;
+
+mod errors;
+mod procstat;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the user who runs the daemon.
+    #[clap(short, long, value_parser)]
+    user: String,
+    /// Location of the std output file to use
+    #[clap(short, long, value_parser, default_value = "/tmp/n8w8-agent.log")]
+    outfile: String,
+    /// Location of the std err file to use
+    #[clap(short, long, value_parser, default_value = "/tmp/n8w8-agent-error.log")]
+    errfile: String,
+    /// Location of the std output file to use
+    #[clap(short, long, value_parser, default_value = "/tmp/n8w8-agent.pid")]
+    pidfile: String,
+    /// The work dir
+    #[clap(short, long, value_parser, default_value = "/tmp")]
+    workdir: String,
+    /// Refresh timeout for the agent query loop.
+    #[clap(short, long, value_parser, default_value_t = 5000)]
+    refresh: u64,
+}
+
+#[cfg(not(windows))]
 fn main() {
-    println!("Hello, world!");
+    // Parse args first ;)
+    let args = Args::parse();
+    let stdout = File::create(args.outfile.as_str()).unwrap();
+    let stderr = File::create(args.errfile.as_str()).unwrap();
+    let sleep_time = time::Duration::from_millis(args.refresh);
+    let p = Path::new(args.pidfile.as_str());
+
+    //OK, we can start
+    println!("Starting N8w8 Agent..");
+    println!(
+        "Will write logs to {}, errors to {}, and pid should be at {}, timeout of {}ms",
+        args.outfile, args.errfile, args.pidfile, args.refresh
+    );
+    let daemonize = Daemonize::new()
+        .pid_file(p) // Every method except `new` and `start`
+        // .chown_pid_file(true) // is optional, see `Daemonize` documentation
+        .working_directory(Path::new(args.workdir.as_str())) // for default behaviour.
+        .user(args.user.as_str())
+        // .group("adm") // Group name
+        // .group(2) // or group id.
+        // .umask(0o777) // Set umask, `0o027` by default.
+        .stdout(stdout) // Redirect stdout to `/tmp/daemon.out`.
+        .stderr(stderr) // Redirect stderr to `/tmp/daemon.err`.
+        .exit_action(|| println!("Should be running now. Please check via pid file! :)"))
+        .privileged_action(|| println!("Will enter loop now.."));
+
+    match daemonize.start() {
+        Ok(_) => {
+            println!("Starting loop..");
+            let mut sys = System::new_all();
+            loop {
+                let now: DateTime<Utc> = Utc::now();
+                sys.refresh_all();
+                let date = format!("UTC now is: {}", now);
+                println!("Date is now {}", date);
+                let mut disk_vec: Vec<AgentDiscData> = Vec::new();
+                for disk in sys.disks() {
+                    let this_disk_data = AgentDiscData {
+                        device: disk.name().to_str().unwrap().to_string(),
+                        mountpoint: disk.mount_point().to_str().unwrap().to_string(),
+                        max_storage: disk.total_space(),
+                        free_storage: disk.available_space(),
+                        special_fields: Default::default(),
+                    };
+                    disk_vec.push(this_disk_data);
+                }
+
+                let cpu_proc_stats = parse_proc_stat().expect("TODO: panic message");
+                info!("This machine has {} cores!", cpu_proc_stats.len());
+                let agent_node_data = AgentNodeData {
+                    hostname: sys.host_name().unwrap(),
+                    load1: sys.load_average().one,
+                    load5: sys.load_average().five,
+                    load15: sys.load_average().fifteen,
+                    totalMemory: sys.total_memory(),
+                    usedMemory: sys.used_memory(),
+                    freeMemory: sys.free_memory(),
+                    totalSwap: sys.total_swap(),
+                    usedSwap: sys.used_swap(),
+                    freeSwap: sys.free_swap(),
+                    kernelversion: sys.kernel_version().unwrap(),
+                    cpudata: cpu_proc_stats,
+                    disks: disk_vec,
+                    os_name: sys.name().unwrap(),
+                    os_version: sys.os_version().unwrap(),
+                    special_fields: Default::default(),
+                };
+                println!("Node data is {}", agent_node_data);
+                thread::sleep(sleep_time);
+            }
+        }
+        Err(e) => eprintln!("Error occurred: {}", e),
+    }
 }
