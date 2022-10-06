8df7c1ded89ce6606b5ceac9de0a43b8e947495a
Dirk <472167-dstrauss@users.noreply.gitlab.com>
2022-10-06 18:04:14 +0200

    🔨 pause exec

--- createReleaseTag.sh
+++ createReleaseTag.sh
@@ -4,11 +4,21 @@ TAG=${1:-notag}
 
 cargo install cargo-edit
 export CURRENTDATE="$(date)"
-export SEMVER=$(echo $TAG | grep -Eo "(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")
+export SEMVER=$(echo $TAG | grep -Po "(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")
 
 if [[ -z "${SEMVER}" ]]; then
-  echo "No Semver: $TAG"
+  echo "No Semver: $TAG -> \"${SEMVER}\""
   exit 1
+else
+  echo "Will use version $SEMVER"
+  while [ true ]; do
+    read -t 3 -n 1
+    if [ $? = 0 ]; then
+      exit
+    else
+      echo "waiting for the keypress"
+    fi
+  done
 fi
 
 cargo set-version --workspace "${SEMVER}"
